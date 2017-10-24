f0e3201085bfecaa5ea9afc6ceeabfb7785f9311
Vit Benton <Vitek@Vits-MacBook-Air.local>
2017-10-24 18:33:50 -0400

    added link to article source, also rearranged scrape path to include validation and render page asynchronously

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,6 +15,8 @@ module.exports = function(app) {
             // Then, we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
 
+            var resultArr = [];
+
             // Now, we grab every h2 within an article tag, and do the following:
             $(".postlist__item").each(function(i, element) {
                 // Save an empty result object
@@ -31,35 +33,45 @@ module.exports = function(app) {
                 result.link = $(this)
                     .find("h1.headline")
                     .children("a")
-                    .attr("href")
+                    .attr("href");
 
-                // Create a new Article using the `result` object built from scraping
-                db.Article
-                    .create(result)
-                    .then(function(dbArticle) {
-                        // If we were able to successfully scrape and save an the articles, render page with all articles
-                        console.log("Web Scrape Complete")
-                    })
-                    .catch(function(err) {
-                        // If an error occurred, send it to the client
-                        console.log(err);
-                    });
+                if (result.headline == "" || result.summary == "" || result.link == "") {//validation for empty properties
+                    console.log("Article skipped due to empty headline, summary, or link");
+                } else {
+                    resultArr.push(result);//push to array of results if headline, summary, and link are provided
+                }
 
             });
-        }); //after here
 
-        db.Article
-            .find({})
-            // ..and populate all of the comments associated with it
-            .populate("comments")
-            .then(function(dbArticle) {
-                // render results into handelbars view
-                res.render("index", {articles: dbArticle});
-            })
-            .catch(function(err) {
-                // If an error occurred, send it to the client
-                console.log(err);
-            });
+            // Create a new Article using the `result` object built from scraping
+            db.Article
+                .create(resultArr)
+                .then(function(dbArticle) {
+                    // If we were able to successfully scrape and save an the articles, render page with all articles
+                    console.log("Web Scrape Complete");
+
+                    db.Article
+                        .find({})
+                        // ..and populate all of the comments associated with it
+                        .populate("comments")
+                        .then(function(dbArticle) {
+                            // render results into handelbars view
+                            res.render("index", { articles: dbArticle });
+                        })
+                        .catch(function(err) {
+                            // If an error occurred, send it to the client
+                            console.log(err);
+                        });
+
+                })
+                .catch(function(err) {
+                    // If an error occurred, send it to the client
+                    console.log(err);
+                });
+
+
+        });
+
     });
 
 };
