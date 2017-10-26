42e144afbed2974d12b9139ed00c8046f5872e16
Vit Benton <vitbenton@Macintosh.local>
2017-10-25 21:19:58 -0400

    inserted delete button for each comment, set up AJAX delete call, still need to set up route for deletion

--- public/js/app.js
+++ public/js/app.js
@@ -68,6 +68,30 @@ $(document).ready(function() {
 
     });
 
+    //event listener for button click to delete comments;
+    $('body').on('click', '.viewComments', function() {
+
+        event.preventDefault();
+
+        var commentId: $(this).attr("data-commentId");
+
+        var commentToDelete = {
+            commentId: $(this).attr("data-commentId")
+        };
+
+        $.delete("/delete", commentToDelete)
+                .done(function(data) {
+
+                    if (data === true) {
+                        alert("Comment Successfully Deleted!");
+                        window.location = "/";
+                    } else {
+                        alert("There Was An Error Deleting Your Comment!")
+                    };
+
+                });
+
+    });
 
 
 }); //END OF $(document).ready
